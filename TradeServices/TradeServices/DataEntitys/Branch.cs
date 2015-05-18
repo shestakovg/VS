7f5a4e1d3160178cea8584faf8584b5d2b9a1c04
Shestakov Gennadiy <shestakov.g@gmail.com>
2015-05-18 18:31:15 +0300

    18:31

---
+++ TradeServices/TradeServices/DataEntitys/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TradeServices.DataEntitys
+{
+    [DataContract]
+    public class Branch
+    {
+        [DataMember(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
---
+++ TradeServices/TradeServices/DataEntitys/Route.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TradeServices.DataEntitys
+{
+    [DataContract]
+    public class Route
+    {
+        [DataMember(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "Description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "Code")]
+        public string Code { get; set; }
+
+        [DataMember(Name = "Employee")]
+        public string Employee { get; set; }
+    }
+}
