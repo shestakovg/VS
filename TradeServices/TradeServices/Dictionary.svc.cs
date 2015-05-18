428a0f5d723ef7c98deed355b8dcfd9ce9bbc165
Shestakov Gennadiy <shestakov.g@gmail.com>
2015-05-18 19:08:18 +0300

    19:08

---
+++ TradeServices/TradeServices/Dictionary.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TradeServices.Classes;
+using TradeServices.DataEntitys;
+
+namespace TradeServices.SVC
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Dictionary" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Dictionary.svc or Dictionary.svc.cs at the Solution Explorer and start debugging.
+    public class Dictionary : IDictionary
+    {
+        public  Branch[] GetBranch()
+        {
+            _1CBranches branches = new _1CBranches();
+            return branches.ConvertToArray() as Branch[];
+        }
+    }
+}
