bfb6b47e2f920111d0f728e3939b8325fe69b2a5
shestakovg <shestakov.ggGmail.com>
2015-05-29 17:06:06 +0300

    29.05

---
+++ TradeServices/TradeServices/DataEntitys/Contract.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TradeServices.DataEntitys
+{
+    public class Contract
+    {
+
+        [DataMember(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "Description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "PriceId")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "Price")]
+        public string Description { get; set; }
+        
+        [DataMember(Name = "Price")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "LimitSum")]
+        public double LimitSum { get; set; }
+
+        [DataMember(Name = "Reprieve")]
+        public int Reprieve { get; set; }
+    }
+}
