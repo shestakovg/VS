a630d587029a204a8bbc5ebeb67d4b2fcb851ea4
Shestakov Gennadiy <shestakovg>
2015-06-01 23:39:16 +0300

    01.06.15 2300

--- TradeServices/TradeServices/DataEntitys/Contract.cs
+++ TradeServices/TradeServices/DataEntitys/Contract.cs
@@ -8,7 +8,9 @@ namespace TradeServices.DataEntitys
 {
     public class Contract
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
         [DataMember(Name = "Id")]
         public Guid Id { get; set; }
 
@@ -16,13 +18,13 @@ namespace TradeServices.DataEntitys
         public string Description { get; set; }
 
         [DataMember(Name = "PriceId")]
-        public Guid Id { get; set; }
+        public Guid PriceId { get; set; }
 
         [DataMember(Name = "Price")]
-        public string Description { get; set; }
-        
-        [DataMember(Name = "Price")]
-        public string Description { get; set; }
+        public string Price { get; set; }
+
+        [DataMember(Name = "PriceDescription")]
+        public string PriceDescription { get; set; }
 
         [DataMember(Name = "LimitSum")]
         public double LimitSum { get; set; }
