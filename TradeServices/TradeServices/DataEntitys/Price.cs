3c69ad0e3eb2550394a2a7bcdcf15505613a365a
shestakovg <shestakov.ggGmail.com>
2015-06-05 14:30:51 +0300

    05.06.15 price2

---
+++ TradeServices/TradeServices/DataEntitys/Price.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TradeServices.DataEntitys
+{
+     [DataContract]
+    public class Price
+    {
+        [DataMember(Name = "SkuId")]
+        public Guid SkuId { get; set; }
+
+        [DataMember(Name = "PriceId")]
+        public Guid PriceId { get; set; }
+
+       [DataMember(Name = "Pric")]
+        public double Pric { get; set; }
+     }
+}
