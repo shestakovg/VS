358290a65279cbb1e5f3f5324238a573b6c824fa
Shestakov Gennadiy <shestakov.g@gmail.com>
2015-06-12 21:00:16 +0300

    start proccess orders

---
+++ TradeServices/TradeServices/DataEntitys/OrderPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TradeServices.DataEntitys
+{
+    public class OrderPosition
+    {
+        private Guid skuId;
+
+        public Guid SkuId
+        {
+            get { return skuId; }
+            set { skuId = value; }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        
+        public OrderPosition(Guid skuId, int quantity)
+        {
+            this.skuId = skuId;
+            this.quantity = quantity;
+        }
+    }
+}
