5cc8b87ac8d53dd6224343345f05b391146fce69
gs <shestakov.g@hotmail.com>
2017-02-27 16:22:54 +0200

    final

---
+++ TradeServices/UnicomWeb/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace UnicomWeb.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; }
+        public ApplicationUser()
+        {
+
+        }
+    }
+}
